bb5c627c4b47c8fbaaba79941659a166ea3f0d91
Maycon Douglas <maycondbraga.s@gmail.com>
2021-10-23 01:52:36 -0300

    refactor: enrollments view and course delete view

--- Leyre.University.Web/Controllers/EnrollmentsController.cs
+++ Leyre.University.Web/Controllers/EnrollmentsController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Leyre.University.Business.Interfaces;
 using Leyre.University.Dto.Dtos;
+using Leyre.University.Model.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace Leyre.University.Web.Controllers
 {
     public class EnrollmentsController : Controller
     {
-        private IEnrollmentBusiness enrollmentBusiness;
+        private readonly IEnrollmentBusiness enrollmentBusiness;
         private readonly IMapper mapper;
 
         public EnrollmentsController(IEnrollmentBusiness enrollmentBusiness, IMapper mapper)
@@ -25,6 +26,113 @@ namespace Leyre.University.Web.Controllers
             var listEnrollments = dataEnrollments.Select(x => mapper.Map<EnrollmentDto>(x)).ToList();
 
             return View(listEnrollments);
+        }
+
+        // GET: Enrollments/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Enrollments/Create
+        [HttpPost]
+        public async Task<IActionResult> Create(EnrollmentDto enrollmentDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var enrollmentModel = mapper.Map<EnrollmentModel>(enrollmentDto);
+
+                await enrollmentBusiness.Insert(enrollmentModel);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(enrollmentDto);
+        }
+
+        // GET: Enrollments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                NotFound();
+            }
+
+            var enrollmentModel = await enrollmentBusiness.GetById(id.Value);
+
+            if (enrollmentModel == null)
+            {
+                NotFound();
+            }
+
+            var enrollmentDto = mapper.Map<EnrollmentDto>(enrollmentModel);
+
+            return View(enrollmentDto);
+        }
+
+        // POST: Enrollments/Edit/5
+        [HttpPost]
+        public async Task<IActionResult> Edit(EnrollmentDto enrollmentDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var enrollmentModel = mapper.Map<EnrollmentModel>(enrollmentDto);
+
+                await enrollmentBusiness.Update(enrollmentModel);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(enrollmentDto);
+        }
+
+        // GET: Enrollments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var enrollmentModel = await enrollmentBusiness.GetById(id.Value);
+
+            if (enrollmentModel == null)
+            {
+                NotFound();
+            }
+
+            var enrollmentDto = mapper.Map<EnrollmentDto>(enrollmentModel);
+
+            return View(enrollmentDto);
+        }
+
+        // GET: Enrollments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var enrollmentModel = await enrollmentBusiness.GetById(id.Value);
+
+            if (enrollmentModel == null)
+            {
+                NotFound();
+            }
+
+            var enrollmentDto = mapper.Map<EnrollmentDto>(enrollmentModel);
+
+            return View(enrollmentDto);
+        }
+
+        // POST: Enrollments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await enrollmentBusiness.Delete(id);
+
+            return RedirectToAction("Index");
         }
     }
 }
